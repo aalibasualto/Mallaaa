89f5cdaafb847ead4ebd87bec05388928b9db5cd
aalibasualto <145818725+aalibasualto@users.noreply.github.com>
2025-07-14 22:13:06 -0400

    script.js

--- script.js
+++ script.js
@@ -1,72 +1,86 @@
+// script.js
 
 const malla = document.getElementById("malla");
 
 const ramos = [
-  { nombre: "Cálculo I", abre: ["Cálculo II"], semestre: 1 },
-  { nombre: "Álgebra I", abre: ["Álgebra II"], semestre: 1 },
-  { nombre: "Física I", abre: ["Física II"], semestre: 1 },
-  { nombre: "Introducción al Diseño en ingeniería", abre: ["Fundamentos de Programación para Ingeniería"], semestre: 1 },
-  { nombre: "Introducción a la Ingeniería Industrial", abre: [], semestre: 1 },
+  // Nivel 1
+  { nombre: "CÁLCULO I PARA INGENIERÍA", codigo: "10138", semestre: 1, abre: ["CÁLCULO II PARA INGENIERÍA"] },
+  { nombre: "ÁLGEBRA I PARA INGENIERÍA", codigo: "10139", semestre: 1, abre: ["ÁLGEBRA II PARA INGENIERÍA"] },
+  { nombre: "FÍSICA I PARA INGENIERÍA", codigo: "10140", semestre: 1, abre: ["FÍSICA II PARA INGENIERÍA"] },
+  { nombre: "INTRODUCCIÓN AL DISEÑO EN INGENIERÍA", codigo: "10141", semestre: 1, abre: ["FUNDAMENTOS DE PROGRAMACIÓN PARA INGENIERÍA"] },
 
-  { nombre: "Cálculo II", abre: ["Cálculo III"], req: ["Cálculo I"], semestre: 2 },
-  { nombre: "Álgebra II", abre: ["Ecuaciones Diferenciales", "Investigación de Operaciones"], req: ["Álgebra I"], semestre: 2 },
-  { nombre: "Física II", abre: ["Física Moderna"], req: ["Física I"], semestre: 2 },
-  { nombre: "Fundamentos de Programación para Ingeniería", abre: ["Programación"], req: ["Introducción al Diseño en ingeniería"], semestre: 2 },
-  { nombre: "Química general y Termodinámica", abre: ["Ingeniería de Sistemas", "Operaciones y Procesos Industriales"], semestre: 2 },
+  // Nivel 2
+  { nombre: "CÁLCULO II PARA INGENIERÍA", codigo: "10142", semestre: 2, req: ["CÁLCULO I PARA INGENIERÍA"], abre: ["CÁLCULO III PARA INGENIERÍA"] },
+  { nombre: "ÁLGEBRA II PARA INGENIERÍA", codigo: "10143", semestre: 2, req: ["ÁLGEBRA I PARA INGENIERÍA"], abre: ["ECUACIONES DIFERENCIALES PARA INGENIERÍA"] },
+  { nombre: "FÍSICA II PARA INGENIERÍA", codigo: "10144", semestre: 2, req: ["FÍSICA I PARA INGENIERÍA"], abre: ["FÍSICA MODERNA"] },
+  { nombre: "FUNDAMENTOS DE PROGRAMACIÓN PARA INGENIERÍA", codigo: "10145", semestre: 2, req: ["INTRODUCCIÓN AL DISEÑO EN INGENIERÍA"], abre: ["PROGRAMACIÓN"] },
 
-  { nombre: "Inglés I", abre: ["Inglés II"], semestre: 3 },
-  { nombre: "Cálculo III", abre: ["Métodos Numéricos para Ingeniería", "Investigación de Operaciones"], req: ["Cálculo II"], semestre: 3 },
-  { nombre: "Ecuaciones Diferenciales", abre: ["Métodos Numéricos en Ingeniería de Sistemas"], req: ["Álgebra II"], semestre: 3 },
-  { nombre: "Fundamentos de Economía", abre: ["Taller de Diseño en Ingeniería", "Microeconomía"], semestre: 3 },
-  { nombre: "Programación", abre: ["Diseño Digital Computacional"], req: ["Fundamentos de Programación para Ingeniería"], semestre: 3 },
-  { nombre: "Física Moderna", abre: ["Operaciones y Procesos Industriales"], req: ["Física II"], semestre: 3 },
+  // Nivel 3
+  { nombre: "INGLÉS I", codigo: "10130", semestre: 3, abre: ["INGLÉS II"] },
+  { nombre: "CÁLCULO III PARA INGENIERÍA", codigo: "10146", semestre: 3, req: ["CÁLCULO II PARA INGENIERÍA"] },
+  { nombre: "ECUACIONES DIFERENCIALES PARA INGENIERÍA", codigo: "10155", semestre: 3, req: ["ÁLGEBRA II PARA INGENIERÍA"] },
+  { nombre: "FUNDAMENTOS DE ECONOMÍA PARA INGENIERÍA", codigo: "10157", semestre: 3 },
 
-  { nombre: "Inglés II", abre: ["Inglés III"], semestre: 4 },
-  { nombre: "Taller de Diseño en Ingeniería", abre: ["Introducción a la Innovación"], req: ["Fundamentos de Economía"], semestre: 4 },
-  { nombre: "Análisis estadístico para ingeniería", abre: ["Estadística Aplicada"], semestre: 4 },
-  { nombre: "Métodos Numéricos para Ingeniería", abre: ["Modelamiento de Sistemas Complejos"], req: ["Cálculo III"], semestre: 4 },
-  { nombre: "Ingeniería de Sistemas", abre: ["Administración", "Taller de Gestión y Liderazgo"], req: ["Química general y Termodinámica"], semestre: 4 },
-  { nombre: "Diseño Digital Computacional", abre: ["Tecnologías para la Gestión"], req: ["Programación"], semestre: 4 },
+  // Nivel 4
+  { nombre: "INGLÉS II", codigo: "10131", semestre: 4, req: ["INGLÉS I"], abre: ["INGLÉS III"] },
+  { nombre: "TALLER DE DISEÑO EN INGENIERÍA", codigo: "10148", semestre: 4, req: ["FUNDAMENTOS DE ECONOMÍA PARA INGENIERÍA"] },
+  { nombre: "ANÁLISIS ESTADÍSTICO PARA INGENIERÍA", codigo: "10158", semestre: 4 },
+  { nombre: "MÉTODOS NUMÉRICOS PARA INGENIERÍA", codigo: "10159", semestre: 4 },
 
-  { nombre: "Inglés III", abre: ["Inglés IV"], semestre: 5 },
-  { nombre: "Estadística Aplicada", abre: ["Marketing Estratégico", "Modelos Estocásticos"], req: ["Análisis estadístico para ingeniería"], semestre: 5 },
-  { nombre: "Operaciones y Procesos Industriales", abre: ["Diseño de Productos y Sistemas Productivos"], req: ["Física Moderna", "Química general y Termodinámica"], semestre: 5 },
-  { nombre: "Administración", abre: ["Contabilidad y Costos", "Marketing Estratégico"], req: ["Ingeniería de Sistemas"], semestre: 5 },
-  { nombre: "Microeconomía", abre: ["Macroeconomía", "Sistemas de Información"], req: ["Fundamentos de Economía"], semestre: 5 },
-  { nombre: "Taller de Gestión y Liderazgo", abre: ["Gestión de Emprendimiento"], req: ["Ingeniería de Sistemas"], semestre: 5 },
+  // Nivel 5
+  { nombre: "INGLÉS III", codigo: "10132", semestre: 5, req: ["INGLÉS II"], abre: ["INGLÉS IV"] },
+  { nombre: "ESTADÍSTICA APLICADA", codigo: "14134", semestre: 5, req: ["ANÁLISIS ESTADÍSTICO PARA INGENIERÍA"] },
+  { nombre: "OPERACIONES Y PROCESOS INDUSTRIALES", codigo: "14135", semestre: 5 },
+  { nombre: "ADMINISTRACIÓN", codigo: "14136", semestre: 5 },
 
-  { nombre: "Inglés IV", abre: [], semestre: 6 },
-  { nombre: "Macroeconomía", abre: ["Sistemas de Información"], req: ["Microeconomía"], semestre: 6 },
-  { nombre: "Contabilidad y Costos", abre: ["Finanzas"], req: ["Administración"], semestre: 6 },
-  { nombre: "Investigación de Operaciones", abre: ["Modelos Estocásticos", "Gestión de Producción de Bienes y Servicios"], req: ["Álgebra II", "Cálculo III"], semestre: 6 },
-  { nombre: "Tecnologías para la Gestión", abre: ["Inteligencia de Negocio"], req: ["Diseño Digital Computacional"], semestre: 6 },
-  { nombre: "Introducción a la Innovación", abre: ["Gestión de Emprendimiento"], req: ["Taller de Diseño en Ingeniería"], semestre: 6 },
+  // Nivel 6
+  { nombre: "INGLÉS IV", codigo: "10133", semestre: 6, req: ["INGLÉS III"] },
+  { nombre: "MACROECONOMÍA", codigo: "14139", semestre: 6 },
+  { nombre: "CONTABILIDAD Y COSTOS", codigo: "14140", semestre: 6 },
+  { nombre: "INVESTIGACIÓN DE OPERACIONES", codigo: "14141", semestre: 6 },
 
-  { nombre: "Diseño de Productos y Sistemas Productivos", abre: ["Gestión de Producción de Bienes y Servicios"], req: ["Operaciones y Procesos Industriales"], semestre: 7 },
-  { nombre: "Marketing Estratégico", abre: ["Inteligencia de Negocio", "Gestión Estratégica"], req: ["Estadística Aplicada", "Administración"], semestre: 7 },
-  { nombre: "Modelos Estocásticos", abre: ["Análisis de Decisiones", "Modelamiento de Sistemas Complejos"], req: ["Estadística Aplicada", "Investigación de Operaciones"], semestre: 7 },
-  { nombre: "Finanzas", abre: ["Evaluación de Proyectos", "Gestión Estratégica"], req: ["Contabilidad y Costos"], semestre: 7 },
-  { nombre: "Gestión de Emprendimiento", abre: ["Gestión de Personas"], req: ["Taller de Gestión y Liderazgo", "Introducción a la Innovación"], semestre: 7 },
+  // Nivel 7
+  { nombre: "DISEÑO DE PRODUCTOS Y SISTEMAS PRODUCTIVOS", codigo: "14144", semestre: 7 },
+  { nombre: "MARKETING ESTRATÉGICO", codigo: "14145", semestre: 7 },
+  { nombre: "MODELO ESTOCÁSTICOS", codigo: "14146", semestre: 7 },
+  { nombre: "FINANZAS", codigo: "14147", semestre: 7 },
 
-  { nombre: "Sistemas de Información", abre: ["Electivo I", "Gestión de Cadena de Suministro"], req: ["Macroeconomía", "Microeconomía"], semestre: 8 },
-  { nombre: "Análisis de Decisiones", abre: ["Tópico de Especialidad I"], req: ["Modelos Estocásticos"], semestre: 8 },
-  { nombre: "Modelamiento de Sistemas Complejos", abre: [], req: ["Métodos Numéricos para Ingeniería", "Modelos Estocásticos"], semestre: 8 },
-  { nombre: "Inteligencia de Negocio", abre: [], req: ["Tecnologías para la Gestión", "Marketing Estratégico"], semestre: 8 },
-  { nombre: "Evaluación de Proyectos", abre: ["Electivo I"], req: ["Finanzas"], semestre: 8 },
+  // Nivel 8
+  { nombre: "SISTEMAS DE INFORMACIÓN", codigo: "14149", semestre: 8 },
+  { nombre: "ANÁLISIS DE DECISIONES", codigo: "14150", semestre: 8 },
+  { nombre: "MODELAMIENTO DE SISTEMAS COMPLEJOS", codigo: "14151", semestre: 8 },
+  { nombre: "INTELIGENCIA DE NEGOCIO", codigo: "14152", semestre: 8 },
 
-  { nombre: "Tópico de Especialidad I", abre: ["Tópico de Especialidad II", "Proyecto de Ingeniería"], req: ["Análisis de Decisiones"], semestre: 9 },
-  { nombre: "Gestión de Personas", abre: ["Proyecto de Ingeniería"], req: ["Gestión de Emprendimiento"], semestre: 9 },
-  { nombre: "Gestión Estratégica", abre: ["Proyecto de Ingeniería"], req: ["Marketing Estratégico", "Finanzas"], semestre: 9 },
-  { nombre: "Electivo I", abre: ["Proyecto de Ingeniería", "Tópico de Especialidad III", "Electivo II"], req: ["Evaluación de Proyectos", "Sistemas de Información"], semestre: 9 },
-  { nombre: "Gestión de Producción de Bienes y Servicios", abre: ["Proyecto de Ingeniería", "Gestión de Cadena de Suministro"], req: ["Diseño de Productos y Sistemas Productivos", "Investigación de Operaciones"], semestre: 9 },
+  // Nivel 9
+  { nombre: "TÓPICO DE ESPECIALIDAD I", codigo: "14154", semestre: 9 },
+  { nombre: "GESTIÓN DE PERSONAS", codigo: "14155", semestre: 9 },
+  { nombre: "GESTIÓN ESTRATÉGICA", codigo: "14157", semestre: 9 },
+  { nombre: "ELECTIVO I", codigo: "14158", semestre: 9 },
 
-  { nombre: "Tópico de Especialidad II", abre: ["Trabajo de Titulación"], req: ["Tópico de Especialidad I"], semestre: 10 },
-  { nombre: "Proyecto de Ingeniería", abre: ["Trabajo de Titulación"], req: ["Tópico de Especialidad I", "Gestión de Personas", "Gestión Estratégica", "Electivo I", "Gestión de Producción de Bienes y Servicios"], semestre: 10 },
-  { nombre: "Gestión de Cadena de Suministro", abre: ["Trabajo de Titulación"], req: ["Sistemas de Información", "Gestión de Producción de Bienes y Servicios"], semestre: 10 },
-  { nombre: "Tópico de Especialidad III", abre: ["Trabajo de Titulación"], req: ["Electivo I"], semestre: 10 },
-  { nombre: "Electivo II", abre: ["Trabajo de Titulación"], req: ["Electivo I"], semestre: 10 },
+  // Nivel 10
+  { nombre: "TÓPICO DE ESPECIALIDAD II", codigo: "14159", semestre: 10, req: ["TÓPICO DE ESPECIALIDAD I"] },
+  { nombre: "PROYECTO DE INGENIERÍA", codigo: "14160", semestre: 10 },
+  { nombre: "GESTIÓN DE CADENA DE SUMINISTRO", codigo: "14161", semestre: 10 },
+  { nombre: "TÓPICO DE ESPECIALIDAD III", codigo: "14162", semestre: 10 },
 
-  { nombre: "Trabajo de Titulación", abre: [], semestre: 11 }
+  // Nivel 11
+  { nombre: "TRABAJO DE TITULACIÓN", codigo: "14164", semestre: 11 },
+
+  // Otros ramos
+  { nombre: "INTRODUCCIÓN A LA INGENIERÍA INDUSTRIAL", codigo: "14125", semestre: 1 },
+  { nombre: "QUÍMICA GENERAL Y TERMODINÁMICA", codigo: "14126", semestre: 2 },
+  { nombre: "PROGRAMACIÓN", codigo: "14129", semestre: 3 },
+  { nombre: "INGENIERÍA DE SISTEMAS", codigo: "14132", semestre: 4 },
+  { nombre: "MICROECONOMÍA", codigo: "14137", semestre: 5 },
+  { nombre: "TECNOLOGÍAS PARA LA GESTIÓN", codigo: "14142", semestre: 6 },
+  { nombre: "GESTIÓN DE EMPRENDIMIENTO", codigo: "14148", semestre: 7 },
+  { nombre: "EVALUACIÓN DE PROYECTOS", codigo: "14153", semestre: 8 },
+  { nombre: "GESTIÓN DE PRODUCCIÓN DE BIENES Y SERVICIOS", codigo: "14167", semestre: 9 },
+  { nombre: "ELECTIVO II", codigo: "14163", semestre: 10 },
+  { nombre: "FÍSICA MODERNA", codigo: "14165", semestre: 3 },
+  { nombre: "DISEÑO DIGITAL COMPUTACIONAL", codigo: "14166", semestre: 4 },
+  { nombre: "TALLER DE GESTIÓN Y LIDERAZGO", codigo: "14138", semestre: 5 },
+  { nombre: "INTRODUCCIÓN A LA INNOVACIÓN", codigo: "14143", semestre: 6 }
 ];
 
 const estado = {};
@@ -75,7 +89,7 @@ function crearRamos() {
   ramos.forEach(ramo => {
     const div = document.createElement("div");
     div.className = "ramo";
-    div.textContent = ramo.nombre;
+    div.innerHTML = `<strong>${ramo.codigo}</strong><br>${ramo.nombre}`;
     div.dataset.nombre = ramo.nombre;
     if (ramo.req && ramo.req.length > 0) {
       div.classList.add("bloqueado");
@@ -99,24 +113,26 @@ function actualizarEstado() {
 }
 
 malla.addEventListener("click", e => {
-  if (!e.target.classList.contains("ramo")) return;
-  if (e.target.classList.contains("bloqueado")) return;
+  if (!e.target.closest(".ramo")) return;
+  const div = e.target.closest(".ramo");
+  if (div.classList.contains("bloqueado")) return;
 
-  const nombre = e.target.dataset.nombre;
+  const nombre = div.dataset.nombre;
   estado[nombre] = true;
-  e.target.classList.add("aprobado");
-  e.target.classList.remove("bloqueado");
+  div.classList.add("aprobado");
+  div.classList.remove("bloqueado");
 
-  const abre = ramos.find(r => r.nombre === nombre).abre;
+  const abre = ramos.find(r => r.nombre === nombre).abre || [];
   abre.forEach(abreNombre => {
-    const div = document.querySelector(`[data-nombre="${abreNombre}"]`);
-    if (div) {
-      const requisitos = ramos.find(r => r.nombre === abreNombre).req || [];
-      const puedeActivarse = requisitos.every(r => estado[r]);
-      if (puedeActivarse) div.classList.remove("bloqueado");
+    const divAbre = document.querySelector(`[data-nombre="${abreNombre}"]`);
+    if (divAbre) {
+      const reqs = ramos.find(r => r.nombre === abreNombre).req || [];
+      const puedeActivarse = reqs.every(r => estado[r]);
+      if (puedeActivarse) divAbre.classList.remove("bloqueado");
     }
   });
   actualizarEstado();
 });
 
 crearRamos();
+
